c13bff3d3d20ed1315b2ac9d613b09a4ecb03451
Rajiv Ranjan <ranjan.rajiv00@gmail.com>
2018-03-04 15:14:50 +0530

    fixed stack over flow issue

--- src/sumLargeNumbers.ts
+++ src/sumLargeNumbers.ts
@@ -1,13 +1,13 @@
 class SumLargeNumbers {
     constructor() { }
 
-    private static add = (number1: string[], number2: string[], accumulator: string = '', carry: number = 0): string => {
+    private static add = (number1: Array<string>, number2: Array<string>, accumulator: string = '', carry: number = 0): string => {
         if (!(number1.length || number1.length || carry))
             return accumulator.replace(/^0+/, '');
 
         carry = ~~carry + (~~number1.pop() + ~~number2.pop());
         accumulator = carry % 10 + accumulator;
-        carry = carry > 9 ? 0 : 1;
+        carry = carry > 9 ? 1 : 0;
 
         return SumLargeNumbers.add(number1, number2, accumulator, carry);
     }
